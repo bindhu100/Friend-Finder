d6e23add12d6d101df0497b76679a2b0ba776736
Bindhu <biji_100@yahoo.com>
2019-03-21 17:00:26 -0600

    update changes

---
+++ app/data/friends.js
@@ -0,0 +1,22 @@
+var friends = [{
+  "name":"DAISY",
+  "photo":"https://dummyimage.com/600x400/8f6d8f/ba0d58.jpg&text=Happy",
+  "scores":[5,1,4,4,5,1,2,5,4,1]
+
+},{
+"name":"OLIVIA",
+  "photo":"https://dummyimage.com/600x400/red/1826f0.jpg&text=happy",
+  "scores":[5,2,4,4,6,1,5,5,4,1]
+},{
+  "name":"AMELIA",
+  "photo":"https://dummyimage.com/600x400/8f6d8f/ba0d58.jpg&text=Happy",
+  "scores":[5,2,4,4,6,1,5,5,4,1]
+},{
+  "name":"EMILY",
+  "photo":"https://dummyimage.com/600x400/24d62a/0d14e0.jpg&text=Happy",
+  "scores":[5,2,4,4,6,1,5,5,4,1]
+}
+
+]
+
+module.exports = friends;
---
+++ server.js
@@ -0,0 +1,40 @@
+// var bodyParser = require('body-parser');
+var express = require('express');
+var path = require("path");
+var friends = require('./app/data/friends.js');
+// express make eassy for as using node js and we are setting variable app = to the function express so we can eassly call it
+var app = express();
+// whatever plot is define by the deployement side like heruku or 8080. This will work on local host and we don't have to reconfigure it whenever we have to deploy it in live
+var PORT = process.env.PORT || 8080;
+ 
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+ 
+
+
+// parse application/x-www-form-urlencoded json form,it's is eassy to manipulate
+// bodyParser - is allow us to recive our information back in 
+
+// app.use(bodyParser.urlencoded({ extended: false }))
+ 
+// parse application/json
+
+// app.use(bodyParser.json())
+
+
+ 
+
+// api routing - from api_routs.js
+
+require("./app/routing/apiRoutes.js")(app);
+// to navigate html file - from html_routs.js
+require("./app/routing/htmlRoutes.js")(app);
+
+
+
+app.listen(PORT, function(){
+  console.log("App listing to PORT:"+PORT);
+});
