d1b4b151dee9d7fdee1d6ec3fe0f6dde2824c743
Bindhu <biji_100@yahoo.com>
2019-03-24 18:22:39 -0600

    update changes

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # Friend-Finder
-Friend Finder - Node and Express Servers
+
+<h1>:sparkling_heart:Friend Finder!</h1>
+<h3>Built With</h3>
+
+<ul>
+<li>JavaScript</li>
+<li>jQuery</li>
+<li>Node.js</li>
+<li>AJAX</li>
+<li>Express</li>
+<li>HTML</li>
+</ul>
+
+
+<h3>Overview</h3> 
+In this activity, build a compatibility-based "FriendFinder" application, basically a dating app. This full-stack site will take in results from users' surveys, then compare their answers with those from other users. The app will then display the name and picture of the user with the best overall match.
+
+<h3>Home Page</h3>
+
+
+
+![GitHub Logo](public/image/homePage.PNG)
--- app/data/friends.js
+++ app/data/friends.js
@@ -6,14 +6,14 @@ var friends = [{
 },{
 "name":"OLIVIA",
   "photo":"http://alfa-trening.ru/wp-content/uploads/2014/08/zhenshhina-i-tsvetyi-2.jpg",
-  "scores":[5,3,4,2,5,1,1,5,3,1]
+  "scores":[3,5,4,3,1,4,5,3,4,5]
 },{
   "name":"Tom",
   "photo":"https://images.unsplash.com/photo-1497316730643-415fac54a2af?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80",
   "scores":[1,3,4,5,1,6,2,5,1,1]
 },{
   "name":"EMILY",
-  "photo":"https://palaciodaarte.vteximg.com.br/arquivos/ids/203502-300-300/sdsxv-094-compressed.jpg?v=636601840965330000",
+  "photo":"http://1.bp.blogspot.com/-dNvgygUycDQ/Tq8p4SUphpI/AAAAAAAABjo/UI0IJKbbsbk/s1600/1.jpg",
   "scores":[1,3,4,3,7,2,5,1,3,5]
 }
 
